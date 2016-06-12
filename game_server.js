c264334f856a1141b368468a727658bbee38f32e
Omar <gagathos@thecommotion.com>
2016-06-11 23:52:14 -0400

    Created testing client, react.js implementation

--- game_server.js
+++ game_server.js
@@ -1,4 +1,5 @@
 var http = require('http');
+const fs = require('fs');
 var startTime = new Date();
 
 var gameState = {
@@ -7,15 +8,51 @@ var gameState = {
 }
 
 http.createServer(function(req, res) {
-	
-	res.writeHead(200, {'Content-Type': 'text/json'});
-	res.write(JSON.stringify(gameState));
-	res.end();
-}).listen(8124, "127.0.0.1");
+	console.log("Request received");
+	//we only want to serve game states from this script.  Front-end/web pages etc can be served from any other code, it doesn't matter.
+	//we may want to host a helper javascript code here though that works well with this library.
+	switch(req.method){
+	case "GET":	
+	//if(require('url', true).parse(req.url).)
+	var url = require('url').parse(req.url, true);
+	var query = url.query;
+	if(query.html == "true"){
+		console.log('serving html');
+				res.writeHead(200, {'Content-Type': 'text/html'});
+	 			
+				var index = fs.readFileSync('test.html')
+				res.write(index);
+        res.end();
+	} else if (url.pathname.split('.').pop() == "png" ) {
+	//	console.log(url);
+			console.log('serving image');
+				res.writeHead(200, {'Content-Type': 'image/png'});
+	 			
+				var index = fs.readFileSync(url.pathname.substring(1)); //This is necessary because of slash in url object pathname
+				res.write(index);
+        res.end();
+		
+	} else {
+		res.writeHead(200, {'Content-Type': 'text/json'});
+		res.write(JSON.stringify(gameState));
+		res.end();
+	}
+	break;
+	case "POST":
+	//here we are receiving a command or input from the client
+	break;
+	case "DELETE":
+	//not sure if we want to implement this
+	break;
+	}
+	console.log("Response sent");
+	}).listen(8124, "192.168.1.17");
 console.log('Server running at http://127.0.0.1:8124/');
 
 function gameBoard(width, height){
 	this.board = [];
+	this.width = width;
+	this.height = height;
 	var spaceNumber = 0;
 	for(i = 0; i < width; i++){
 		
@@ -29,19 +66,22 @@ function gameBoard(width, height){
 	this.board.forEach((function(pieces){
 		
 		return function(value){
+		
 			if(value.number % 2 == 0){
 				//if it's in the top 3 rows
-				if(value.y >= height -3) pieces.push(new GamePiece(2,value.x,value.y));
+				var length = pieces.length;
+				if(value.y >= height -3) pieces.push(new GamePiece(2,value.x,value.y,length ));
 				//if it's in the bottom 3 rows
-				if(value.y <= 3) pieces.push(new GamePiece(1, value.x, value.y));
+				if(value.y <= 3) pieces.push(new GamePiece(1, value.x, value.y, length));
 			}
 		}
 	})(this.pieces));
 
 }
-
-function GamePiece(team, currentX, currentY){
+var pieceID = 0;
+function GamePiece(team, currentX, currentY, number){
 	this.team = team;
+	this.number = number;
 	this.currentX = currentX;
 	this.currentY = currentY;
 }
