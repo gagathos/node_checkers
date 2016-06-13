7afb8c90d9e259de5e75200e8c2ef73bcb1af323
Omar <gagathos@thecommotion.com>
2016-06-13 11:32:37 -0400

    Returned host IP to localhost

--- game_server.js
+++ game_server.js
@@ -48,7 +48,7 @@ http.createServer(function(req, res) {
 	break;
 	}
 	console.log("Response sent");
-	}).listen(8124, "192.168.1.17");
+	}).listen(8124, "127.0.0.1");
 console.log('Server running at http://127.0.0.1:8124/');
 
 
